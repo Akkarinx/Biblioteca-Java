db8cd60a5b9d2b4397013cb5c715f3d15bac4bd4
Matheus Silva <m_oliveira9@hotmail.com>
2019-05-05 22:49:29 -0300

    inicio do projeto

---
+++ src/biblioteca/v/pkg2/Pessoa.java
@@ -0,0 +1,16 @@
+
+package biblioteca.v.pkg2;
+
+/**
+ *
+ * @author Matheus Silva
+ * version 1.0
+ */
+public class Pessoa {
+    
+   String cpf, nome;
+   char sexo;
+   int telefone;
+    
+    
+}
